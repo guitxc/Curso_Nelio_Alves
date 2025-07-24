1d3a3ec8f3d91df791808d7f247c85bd90752f1b
guitxc <guisandolidna@gmail.com>
2025-07-24 16:28:10 -0300

    mais exercicos

---
+++ Estrutura sequencial/lista_de_exercicios1/exercicio3.java
@@ -0,0 +1,17 @@
+package lista_de_exercicios1;
+import java.util.Scanner;
+public class exercicio3 {
+    public static void main (String[] args) {
+        // Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto 
+        // de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Veja a diferença de quatros numeros inteiros:");
+        int A, B, C, D, diferenca;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+        D = sc.nextInt();
+        diferenca = (A * B - C * D);
+        System.out.println("A diferença é : " + diferenca);
+    }
+}
